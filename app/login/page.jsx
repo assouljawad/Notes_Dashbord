1183881ec659c9ff48b0bc240879990a4d7151d2
assouljawad <38533122+assouljawad@users.noreply.github.com>
2024-06-05 12:31:24 +0100

    Notes Dashbord

---
+++ app/login/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Page() {
+  return (
+    <div>login Page</div>
+  )
+}
+
+export default Page
---
+++ app/signup/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Page() {
+  return <div>signup page</div>;
+}
+
+export default Page
